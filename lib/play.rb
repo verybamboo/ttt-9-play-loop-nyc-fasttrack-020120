c7d29aad0f218a167a548a2aa5107f0bbdd8cbd9
George Chen <georgechen1338@gmail.com>
2020-02-03 00:31:10 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -25,7 +25,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.chomp
+  input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
